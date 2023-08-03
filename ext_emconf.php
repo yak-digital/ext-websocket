4eab85a1cc71277744740881c02fa7df51d9035f
Tim Kandel <tim.kandel@yak.digital>
2023-08-03 13:48:53 +0200

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Websockets',
+    'description' => '',
+    'category' => 'plugin',
+    'author' => 'Tim Kandel',
+    'author_email' => 'info@yak.digital',
+    'state' => 'alpha',
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-11.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
